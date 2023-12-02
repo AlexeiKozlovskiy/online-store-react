473f524c7cc7b636b12ba8b701087d50350ffcf6
AlexeiKozlovskiy <gekleberefin@gmail.com>
2023-12-02 12:12:54 +0300

    fix: netlify, based on #69

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ssr from 'vite-plugin-ssr/plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), [ssr({ prerender: true })]],
+  plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
