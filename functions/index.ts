fdacdc78f4892773b5e94ca9ed73814cf6c870c2
AlexeiKozlovskiy <gekleberefin@gmail.com>
2023-12-02 10:05:26 +0300

    fix: netlify, based on #69

---
+++ functions/index.ts
@@ -0,0 +1,17 @@
+import { Handler } from '@netlify/functions';
+import { renderPage } from 'vite-plugin-ssr';
+
+export const handler: Handler = async (event) => {
+  const pageContext = await renderPage({
+    url: event.rawUrl,
+    urlOriginal: '',
+  });
+  if (!pageContext.httpResponse) return { statusCode: 200 };
+  console.log(pageContext.httpResponse.statusCode, event.rawUrl);
+
+  return {
+    statusCode: pageContext.httpResponse.statusCode,
+    headers: { 'Content-Type': pageContext.httpResponse.contentType },
+    body: pageContext.httpResponse.body,
+  };
+};
